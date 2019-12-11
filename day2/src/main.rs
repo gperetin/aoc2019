7c2f579de06ce20ef24f879f5be9210839031e2a
Goran Peretin <goran.peretin@gmail.com>
2019-12-10 21:58:31 -0800

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,24 +2,36 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Unable to read inputs");
-    let mut all_codes: Vec<usize> = input.trim().split(',').map(|x| x.parse::<usize>().unwrap()).collect();
+    let mut original_all_codes: Vec<usize> = input.trim().split(',').map(|x| x.parse::<usize>().unwrap()).collect();
 
-    let mut i: usize = 0;
-    loop {
-        let opcode = all_codes[i];
-        if opcode == 99 { break; }
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let mut all_codes = original_all_codes.clone();
+            all_codes[1] = noun;
+            all_codes[2] = verb;
 
-        let first_op = all_codes[all_codes[i+1]];
-        let second_op = all_codes[all_codes[i+2]];
+            let mut ip: usize = 0;
 
-        let res_position = all_codes[i+3];
-        if opcode == 1 {
-            all_codes[res_position] = first_op + second_op;
-        } else if opcode == 2{
-            all_codes[res_position] = first_op * second_op;
+            loop {
+                let opcode = all_codes[ip];
+                if opcode == 99 { break; }
+
+                let first_op = all_codes[all_codes[ip+1]];
+                let second_op = all_codes[all_codes[ip+2]];
+
+                let res_position = all_codes[ip+3];
+                if opcode == 1 {
+                    all_codes[res_position] = first_op + second_op;
+                } else if opcode == 2{
+                    all_codes[res_position] = first_op * second_op;
+                }
+                ip = ip + 4
+            }
+
+            if all_codes[0] == 19690720 {
+                println!("{} {}", noun, verb);
+            }
+
         }
-        i = i + 4
     }
-
-    println!("{:?}", all_codes);
 }
