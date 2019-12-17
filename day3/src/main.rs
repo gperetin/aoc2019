b156b3911d39d576ff35502c4f278231305f8edd
Goran Peretin <goran.peretin@gmail.com>
2019-12-17 10:00:04 -0800

    Part 2 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,50 +1,27 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::fs;
 
-// fn positions_for_move(starting_position: (i32, i32), move_: String) -> Vec<(i32, i32)> {
-//     let direction = move_.remove(0);
-//     let steps = move_.parse::<i32>().unwrap();
-//     let mut all_moves: Vec<(i32, i32)> = Vec::new();
-//     let mut current_position = starting_position;
-//     for i in 1..(steps+1) {
-//         if direction == 'U' {
-//             all_moves.push((current_position.0, current_position.1 + i));
-//             current_position.1 = current_position.1 + i;
-//         } else if direction == 'D' {
-//             all_moves.push((current_position.0, current_position.1 - i));
-//             current_position.1 = current_position.1 - i;
-//         } else if direction == 'L' {
-//             all_moves.push((current_position.0 - i, current_position.1));
-//             current_position.0 = current_position.0 - i;
-//         } else {
-//             all_moves.push((current_position.0 + i, current_position.1));
-//             current_position.0 = current_position.0 + i;
-//         }
-//     }
-
-//     all_moves
-// }
-
-
-fn calculate_positions(moves: Vec<String>) -> HashSet<(i32, i32)> {
+fn calculate_positions(moves: Vec<String>) -> HashMap<(i32, i32), i32> {
     // 'o' is the (0, 0) in the coordinate system
     let mut current_position = (0, 0);
-    let mut wire1_positions = HashSet::new();
+    let mut wire1_positions = HashMap::new();
+    let mut step_no = 0;
     for mut mov in moves {
         let direction = mov.remove(0);
         let steps = mov.parse::<i32>().unwrap();
         for i in 1..(steps+1) {
+            step_no += 1;
             if direction == 'U' {
-                wire1_positions.insert((current_position.0, current_position.1 + 1));
+                wire1_positions.insert((current_position.0, current_position.1 + 1), step_no);
                 current_position.1 = current_position.1 + 1;
             } else if direction == 'D' {
-                wire1_positions.insert((current_position.0, current_position.1 - 1));
+                wire1_positions.insert((current_position.0, current_position.1 - 1), step_no);
                 current_position.1 = current_position.1 - 1;
             } else if direction == 'L' {
-                wire1_positions.insert((current_position.0 - 1, current_position.1));
+                wire1_positions.insert((current_position.0 - 1, current_position.1), step_no);
                 current_position.0 = current_position.0 - 1;
             } else {
-                wire1_positions.insert((current_position.0 + 1, current_position.1));
+                wire1_positions.insert((current_position.0 + 1, current_position.1), step_no);
                 current_position.0 = current_position.0 + 1;
             }
         }
@@ -65,18 +42,17 @@ fn main() {
     let wire1_positions = calculate_positions(wire1);
     let wire2_positions = calculate_positions(wire2);
 
-    let mut min_distance = i32::max_value();
-    let mut closest_point = (0, 0);
+    let mut fewest_steps = i32::max_value();
 
-    for pos in wire2_positions {
-        if wire1_positions.contains(&pos) {
-            if distance_from_center(pos) < min_distance {
-                min_distance = distance_from_center(pos);
-                closest_point = pos;
+    for pos in wire2_positions.keys() {
+        if wire1_positions.contains_key(&pos) {
+            let num_steps = wire1_positions[pos] + wire2_positions[pos];
+            if num_steps < fewest_steps {
+                fewest_steps = num_steps;
             }
         }
     }
 
-    println!("{:?}", distance_from_center(closest_point));
+    println!("{:?}", fewest_steps);
     
 }
